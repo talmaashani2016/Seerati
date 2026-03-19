040c565c5f5b7f2ae47cdd6c96ad71385a5e6ef2
Tariq B AL-Maashani <talmaashani2016@my.fit.edu>
2026-03-20 03:20:02 +0400

    Final

--- dist/client/assets/pdf.worker.min-Cshu3YBm.js
+++
@@ -1,1 +0,0 @@
-const e="/Seerati/assets/pdf.worker.min-B_fnEKel.mjs";export{e as default};
--- dist/server/assets/pdf.worker.min-BWt-5ET3.js
+++
@@ -1,4 +0,0 @@
-const pdf_worker_min = "/Seerati/assets/pdf.worker.min-B_fnEKel.mjs";
-export {
-  pdf_worker_min as default
-};
